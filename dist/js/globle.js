9ee0cc62c2063028b22ffca931eb30f8d7a8cb21
JOsH Lindsay <jlindsay@joshs-mbp.grmsamer.grms.ge.com>
2015-04-01 16:14:15 -0700

    adding navagation

---
+++ dist/js/globle.js
@@ -0,0 +1,27 @@
+var _pages = [
+
+
+    { url: 'index.html' , hash: "" , uuid:"000" },
+
+    { url: 'summary.html' , hash: "" , uuid:"100" },
+        { url: 'home.html' , hash: "" , uuid:"101" },
+
+
+    { url: 'summary.html' , hash: "" , uuid:"200" },
+        { url: 'home.html' , hash: "" , uuid:"201" },
+
+
+    { url: 'summary.html' , hash: "" , uuid:"300" },
+        { url: 'home.html' , hash: "" , uuid:"301" },
+    
+    
+    { url: 'summary.html' , hash: "" , uuid:"400" },
+        { url: 'home.html' , hash: "" , uuid:"401" },
+    
+    
+    { url: 'summary.html' , hash: "" , uuid:"400" },
+        { url: 'home.html' , hash: "" , uuid:"401" },
+
+    { url: 'thankyou.html' , hash: "" , uuid:"600" },
+
+];
--- dist/js/keynote.js
+++ dist/js/keynote.js
@@ -12,117 +12,191 @@
   */
 
 //Keynote
-(function( $ ){
-
-/*
-	$.fn.keynote = function($config)
-	{
-		if ($.data(this, "plugin_gauge")){
-			return;			
-		} 
-
-		return $(this).each(function() {
-		        _instances.push( new GuageClass().init( this,$config ) );
-		        $.data(this, "plugin_gauge");
-		});
-	}
-
-	var _instances = [];
-*/
-	$.fn.keynote = Keynote;
 
 	
-	function Keynote()
-	{
+function Keynote()
+{
 //		console.log("Keynote()");		
 
-		function init($config)
-		{
-			console.log("Keynote.init()");		
-			_config = $config? $config : _config;
-			_pages = $config.pages ? $config.pages : _pages;
-			_index = $config.index ? $config.index : DEFAULT_INDEX;
-			focus( _index );
-		}
-		
-		function reset() {
-		    $.removeCookie('keynote');
-		}
+	function init($config)
+	{
+		console.log("Keynote.init()");		
+		_config = $config? $config : _config;
+		_pages = ( _config.pages )? $config.pages : _pages;
+		_index = ( _config.index )? $config.index : DEFAULT_INDEX;
+//		focus( _index );
+		initKeyBoard();
+	}
 
-		function resetIndex()
-		{
-		    focus(_index);
-		}
+	function initKeyBoard()
+	{
+		console.log("Keynote.initKeyBoard()");	
 
-		function next()
-		{
-		    focus(_index+1);
-		}
+	    var _url = $(location).attr('href');
 
-		function prev()
-		{
-		    focus(_index-1);
-		}
+        try{
+            _params = $.url().param();    
+        }catch(e){
+            //
+        }
+	        
+	        _uuid = _params.uuid;
 
-		function focus(index)
-		{
-		    index = (index >= _pages.length)? 0 : index;
-		    index = (index < 0)? _pages.length-1 : index;
-		    _index = index;
-		    _history.push(_index);
-		    var page = _pages[_index];
-		    //$.removeCookie('navigation');
-		    $.cookie('keynote', _index);
-		    var cookie = $.cookie('keynote');
-		        console.log("focus:index:", _index, ", cookie:",cookie);
+        // for notification
+/*
+        if(typeof _params.has_notification !== 'undefined') {
+            if(_params.has_notification === "true") {
+                $("body").addClass("has_notification");
+            }
+        }
+*/
+	    var _uuidIndex = uuid2index( _uuid );
+	        console.log("uuidIndex : " +  _uuidIndex );
+	        console.log("debug:str:" + _uuid );
+	        console.log("debug:url:" +  _url);
 
+	    var cookie = $.cookie('navigation', Number);
 
-		        // var baseURL = document.location.href.match(/(^[^#]*)/);
-		        // console.log("base: " + baseURL);
+	        _index = Number(cookie);
 
-		        window.location = page.url +"?"+ "uuid="+page.uuid + "&has_notification=" + page.has_notification + "#"+ page.hash ;
-		        $("html, body").animate({ scrollTop: 0 }, "fast");
-		        
-		}
+	        if( _uuidIndex && ( _index != _uuidIndex ) ){
+	            _index = _uuidIndex;
+	            focus(_index);
+	        }
+	       
+	       // handle NaN, must be a blank cookie issue.
+	       if(isNaN(_index)) {
+	            _index = 0;
+	       }
+	    
+	    $(document).keydown(function(e) {
+	        switch(e.keyCode)
+	        {
+	            case 32: // spacebar
+	                next();
+	                console.log("spacebar");
+	                break;
+	            case 39: // right arrow
+	                next();
+	                console.log("right-arrow");
+	                break;
+	            case 37: // left arrow
+	                prev();
+	                console.log("left-arrow");
+	                break;
+	            case 38://up
+	                resetIndex();
+	                break;
+	            case 40://down
+	                resetIndex(); 
+	                break;
+	            case 48: // delete cookie
+	                reset();
+	                _index = 0;
+	                focus(0);
+	            default:
+	                break;
+	        }
+	    });
 
-
-		function uuid2index($uuid)
-		{
-		    for(var i=0; i<_pages.length; i++){
-		        var page = _pages[i];
-		        if( page.uuid == $uuid ){
-		            return i;
-		        }
-		    }
-		    return null;
-		    
-		}
-
-		//interface
-		this.init = init;
-		this.next = next;
-		this.prev = prev;
-		this.focus = focus;
-		this.resetIndex = resetIndex;
-//		this.uuid2index = uuid2index;
-
-		//static vars
-		var DEFAULT_INDEX=0;
-
-		//private var(s)
-		var _config = {};
-		var _uuidIndex;
-		var _params;
-		var _uuid;
-		var _has_notification;
-		var _url="/";
-		var _index = DEFAULT_INDEX;
-		var _history = [];
-
-
+	    $("html, body").animate({ scrollTop: 0 }, "fast");
 	}
 
 
 	
+	function reset() {
+	    $.removeCookie('keynote');
+	}
 
-})( jQuery );
+	function resetIndex()
+	{
+	    focus(_index);
+	}
+
+	function next()
+	{
+	    focus(_index+1);
+	}
+
+	function prev()
+	{
+	    focus(_index-1);
+	}
+
+	function focus($index)
+	{
+		console.log("focus:", $index);
+
+	    var index = ($index >= _pages.length)? 0 : $index;
+	    	index = (index < 0)? _pages.length-1 : index;
+	    	_index = index;
+	    	_history.push(_index);
+	    
+	    var page = _pages[_index];
+/*
+	    if( !page )
+		{
+			return ;
+		}
+*/
+//	    	$.removeCookie('navigation');
+	    	$.cookie('keynote', _index);
+	    var cookie = $.cookie('keynote');
+	        console.log("focus:index:", _index, ", cookie:",cookie);
+
+//	    var baseURL = document.location.href.match(/(^[^#]*)/);
+//	    	console.log("base: " + baseURL);
+		
+		
+			try{
+//				window.location = page.url +"?"+ "uuid="+page.uuid + "&has_notification=" + page.has_notification + "#"+ page.hash ;	
+				window.location = page.url +"?"+ "uuid="+page.uuid + "#"+ page.hash ;	
+				$("html, body").animate({ scrollTop: 0 }, "fast");
+			}catch(e)
+			{
+				//console.log("error: page does not exist" );
+			}
+	        
+	        
+      
+	}
+
+
+	function uuid2index($uuid)
+	{
+	    for(var i=0; i<_pages.length; i++){
+	        var page = _pages[i];
+	        if( page.uuid == $uuid ){
+	            return i;
+	        }
+	    }
+	    return null;
+	    
+	}
+
+	//interface
+	this.init = init;
+	this.next = next;
+	this.prev = prev;
+	this.focus = focus;
+	this.resetIndex = resetIndex;
+//		this.uuid2index = uuid2index;
+
+	//static vars
+	var DEFAULT_INDEX=0;
+
+	//private var(s)
+	var _config = {};
+	var _uuidIndex;
+	var _params;
+	var _uuid;
+	var _has_notification;
+	var _url="/";
+	var _index = DEFAULT_INDEX;
+	var _history = [];
+	var _pages = [];
+
+
+}
+
+
