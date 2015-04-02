20654087e7f578d676e639f0bd662faf616aa916
JOsH Lindsay <jlindsay@joshs-mbp.grmsamer.grms.ge.com>
2015-04-02 14:31:01 -0700

    adding fadeIn, fadeOut when loading templates

--- dist/js/globle.js
+++ dist/js/globle.js
@@ -3,11 +3,11 @@ var _pages = [
     { url: 'index.html' , hash: "" , uuid:"000" },
 
     { url: 'summary.html' , hash: "Section-1" , uuid:"100" },
-        { url: 'home.html' , hash: "" , uuid:"101" },
+        { url: 'home.html' , hash: "tab=fleet" , uuid:"101" },
 
 
     { url: 'summary.html' , hash: "Section-2" , uuid:"200" },
-        { url: 'home.html' , hash: "" , uuid:"201" },
+        { url: 'home.html' , hash: "tab=ship" , uuid:"201" },
 
 
     { url: 'summary.html' , hash: "Section-3" , uuid:"300" },
