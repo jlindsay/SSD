877f46319adc81e4b95f861684dda74f3e8884be
JOsH Lindsay <jlindsay@JOsHs-MacBook-Pro.local>
2015-04-07 16:30:06 -0700

    adding more content

--- dist/js/globle.js
+++ dist/js/globle.js
@@ -3,24 +3,26 @@ var _pages = [
     { url: 'index.html' , hash: "" , uuid:"000" },
 
     { url: 'summary.html' , hash: "Section-1" , uuid:"100" },
-        { url: 'home.html' , hash: "tab=fleet" , uuid:"101" },
+        { url: 'home.html' , hash: "tab=index" , uuid:"101" },
+
+    { url: 'summary.html' , hash: "Section-1" , uuid:"200" },
+        { url: 'home.html' , hash: "tab=fleet" , uuid:"201" },
+
+    { url: 'summary.html' , hash: "Section-2" , uuid:"300" },
+        { url: 'home.html' , hash: "tab=ship" , uuid:"301" },
 
 
-    { url: 'summary.html' , hash: "Section-2" , uuid:"200" },
-        { url: 'home.html' , hash: "tab=ship" , uuid:"201" },
-
-
-    { url: 'summary.html' , hash: "Section-3" , uuid:"300" },
-        { url: 'home.html' , hash: "" , uuid:"301" },
-    
-    
-    { url: 'summary.html' , hash: "Section-4" , uuid:"400" },
+    { url: 'summary.html' , hash: "Section-3" , uuid:"400" },
         { url: 'home.html' , hash: "" , uuid:"401" },
     
     
-    { url: 'summary.html' , hash: "Section-5" , uuid:"500" },
+    { url: 'summary.html' , hash: "Section-4" , uuid:"500" },
         { url: 'home.html' , hash: "" , uuid:"501" },
+    
+    
+    { url: 'summary.html' , hash: "Section-5" , uuid:"600" },
+        { url: 'home.html' , hash: "" , uuid:"601" },
 
-    { url: 'thankyou.html' , hash: "" , uuid:"600" },
+    { url: 'thankyou.html' , hash: "" , uuid:"700" },
 
 ];
