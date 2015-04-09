acb501d4c9bea1d862b78694d18803e21ea28a63
JOsH Lindsay <jlindsay@JOsHs-MacBook-Pro.local>
2015-04-09 13:18:28 -0700

    working on the nab

--- dist/js/globle.js
+++ dist/js/globle.js
@@ -3,13 +3,13 @@ var _pages = [
     { url: 'index.html' , hash: "" , uuid:"000" },
 
     { url: 'summary.html' , hash: "Section-1" , uuid:"100" },
-        { url: 'home.html' , hash: "tab=index" , uuid:"101" },
+        { url: 'home.html' , hash: "tab=ship" , uuid:"101" },
 
     { url: 'summary.html' , hash: "Section-1" , uuid:"200" },
         { url: 'home.html' , hash: "tab=fleet" , uuid:"201" },
 
     { url: 'summary.html' , hash: "Section-2" , uuid:"300" },
-        { url: 'home.html' , hash: "tab=ship" , uuid:"301" },
+        { url: 'home.html' , hash: "" , uuid:"301" },
 
 
     { url: 'summary.html' , hash: "Section-3" , uuid:"400" },
