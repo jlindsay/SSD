c8998433785965bb25ad21f51e034026bbb2a2ba
JOsH Lindsay <joshdlindsay.com@gmail.com>
2015-05-01 12:08:13 -0700

    rearranging the slideshow

---
+++ dist/js/__globle.js
@@ -0,0 +1,58 @@
+var _pages = [
+
+    { url: 'index.html' , hash: "" , uuid:"000" },
+
+    { url: 'summary.html' , hash: "SeaStream™ Insight" , uuid:"010" },
+    { url: 'summary.html' , hash: "Ships" , uuid:"100" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"101" },
+        { url: 'home.html' , hash: "tab=ship&sub=acoustics" , uuid:"102" },
+        { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge" , uuid:"103" },
+        
+            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-1" , uuid:"104" },
+            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-2" , uuid:"105" },
+            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-3" , uuid:"106" },
+            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-4" , uuid:"107" },
+
+
+        { url: 'home.html' , hash: "tab=ship&sub=cbm" , uuid:"108" },
+        { url: 'home.html' , hash: "tab=ship&sub=engine-monitoring" , uuid:"109" },
+        { url: 'home.html' , hash: "tab=ship&sub=fuel" , uuid:"110" },
+        { url: 'home.html' , hash: "tab=ship&sub=gns-reference" , uuid:"111" },
+        
+        { url: 'home.html' , hash: "tab=ship&sub=helideck-and-enviroment" , uuid:"112" },
+        { url: 'home.html' , hash: "tab=ship&sub=integrated-automation" , uuid:"113" },
+        { url: 'home.html' , hash: "tab=ship&sub=load-and-stability" , uuid:"114" },
+        { url: 'home.html' , hash: "tab=ship&sub=navigation" , uuid:"115" },
+        { url: 'home.html' , hash: "tab=ship&sub=oil" , uuid:"116" },
+        { url: 'home.html' , hash: "tab=ship&sub=power-distribution" , uuid:"117" },
+        { url: 'home.html' , hash: "tab=ship&sub=tank-guaging" , uuid:"118" },
+        { url: 'home.html' , hash: "tab=ship&sub=thruster-remote-control" , uuid:"119" },
+        { url: 'home.html' , hash: "tab=ship&sub=water-fresh" , uuid:"120" },
+        { url: 'home.html' , hash: "tab=ship&sub=water-grey" , uuid:"121" },
+
+
+
+    { url: 'summary.html' , hash: "Fleet-View" , uuid:"200" },
+        { url: 'fleet-view.html' , hash: "" , uuid:"201" },
+        { url: 'fleet-view+google-maps.html' , hash: "" , uuid:"210" },
+
+    { url: 'summary.html' , hash: "Section-2" , uuid:"300" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"301" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"302" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"303" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"304" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"305" },
+
+    { url: 'summary.html' , hash: "Shore-Scenario" , uuid:"400" },
+        { url: 'slide-0.html' , hash: "" , uuid:"401" },
+        { url: 'slide-1.html' , hash: "" , uuid:"402" },
+        { url: 'slide-2.html' , hash: "" , uuid:"403" },
+        { url: 'slide-3.html' , hash: "" , uuid:"404" },
+        { url: 'slide-4.html' , hash: "" , uuid:"405" },
+        { url: 'slide-5.html' , hash: "" , uuid:"406" },
+        { url: 'slide-6.html' , hash: "" , uuid:"407" },
+
+
+    { url: 'thankyou.html' , hash: "" , uuid:"500" },
+
+];
--- dist/js/globle.js
+++ dist/js/globle.js
@@ -2,18 +2,22 @@ var _pages = [
 
     { url: 'index.html' , hash: "" , uuid:"000" },
 
-    { url: 'summary.html' , hash: "SeaStream™ Insight" , uuid:"010" },
-    { url: 'summary.html' , hash: "Ships" , uuid:"100" },
+
+    { url: 'summary.html' , hash: "Fleet-View" , uuid:"001" },
+        { url: 'fleet-view.html' , hash: "" , uuid:"002" },
+    
+
+
+    { url: 'summary.html' , hash: "SeaStream™ Insight" , uuid:"100" },
         { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"101" },
-        { url: 'home.html' , hash: "tab=ship&sub=acoustics" , uuid:"102" },
         { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge" , uuid:"103" },
         
-            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-1" , uuid:"104" },
+
+            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge" , uuid:"104" },
             { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-2" , uuid:"105" },
-            { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-3" , uuid:"106" },
             { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge&dropdown=system-4" , uuid:"107" },
 
-
+/*
         { url: 'home.html' , hash: "tab=ship&sub=cbm" , uuid:"108" },
         { url: 'home.html' , hash: "tab=ship&sub=engine-monitoring" , uuid:"109" },
         { url: 'home.html' , hash: "tab=ship&sub=fuel" , uuid:"110" },
@@ -29,12 +33,69 @@ var _pages = [
         { url: 'home.html' , hash: "tab=ship&sub=thruster-remote-control" , uuid:"119" },
         { url: 'home.html' , hash: "tab=ship&sub=water-fresh" , uuid:"120" },
         { url: 'home.html' , hash: "tab=ship&sub=water-grey" , uuid:"121" },
+*/
+
+
+    
+
+
+    { url: 'summary.html' , hash: "Section-2" , uuid:"300" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=overview" , uuid:"301" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"302" },
+        
+
+/*        
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"303" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"304" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"305" },
+*/
+    
+    { url: 'summary.html' , hash: "Ship Scenario" , uuid:"400" },
+        { url: 'slide-3.html' , hash: "" , uuid:"401" },
+        { url: 'slide-4.html' , hash: "" , uuid:"402" },
+        { url: 'slide-5.html' , hash: "" , uuid:"403" },
+        { url: 'slide-6.html' , hash: "" , uuid:"404" },
+
+
+    { url: 'thankyou.html' , hash: "" , uuid:"500" },
+
+];
+
+
+/*
+
+var _pages = [
+    
+    { url: 'index.html' , hash: "" , uuid:"000" },
+    
+
+
+    { url: 'summary.html' , hash: "SeaStream™ Insight" , uuid:"100" },
+        { url: 'slide-0.html' , hash: "" , uuid:"101" },
+        { url: 'slide-1.html' , hash: "" , uuid:"102" },
+        { url: 'slide-2.html' , hash: "" , uuid:"103" },
+
+        { url: 'fleet-view.html' , hash: "" , uuid:"104" },
+        { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"105" },
+        { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"106" },
+        { url: 'home.html' , hash: "tab=ship&sub=ballast-and-bilge" , uuid:"107" },
 
 
 
-    { url: 'summary.html' , hash: "Fleet-View" , uuid:"200" },
-        { url: 'fleet-view.html' , hash: "" , uuid:"201" },
-        { url: 'fleet-view+google-maps.html' , hash: "" , uuid:"210" },
+    { url: 'summary.html' , hash: "Shore Scenario" , uuid:"200" },
+        { url: 'slide-8.html' , hash: "" , uuid:"201" },
+
+
+
+    { url: 'summary.html' , hash: "Ship Scenario" , uuid:"250" },
+        { url: 'slide-3.html' , hash: "" , uuid:"251" },
+        { url: 'slide-4.html' , hash: "" , uuid:"252" },
+        { url: 'slide-5.html' , hash: "" , uuid:"253" },
+        { url: 'slide-6.html' , hash: "" , uuid:"254" },
+        
+
+
+
 
     { url: 'summary.html' , hash: "Section-2" , uuid:"300" },
         { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"301" },
@@ -43,16 +104,11 @@ var _pages = [
         { url: 'home.html' , hash: "tab=ship&sub-tab=drillfloor&sub=home_ship_drillfloor_top-drive" , uuid:"304" },
         { url: 'home.html' , hash: "tab=ship&sub=overview" , uuid:"305" },
 
-    { url: 'summary.html' , hash: "Shore-Scenario" , uuid:"400" },
-        { url: 'slide-0.html' , hash: "" , uuid:"401" },
-        { url: 'slide-1.html' , hash: "" , uuid:"402" },
-        { url: 'slide-2.html' , hash: "" , uuid:"403" },
-        { url: 'slide-3.html' , hash: "" , uuid:"404" },
-        { url: 'slide-4.html' , hash: "" , uuid:"405" },
-        { url: 'slide-5.html' , hash: "" , uuid:"406" },
-        { url: 'slide-6.html' , hash: "" , uuid:"407" },
+
+
 
 
     { url: 'thankyou.html' , hash: "" , uuid:"500" },
 
 ];
+*/
